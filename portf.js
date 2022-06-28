930e83ba3c75df23121ea26e4a97f456f8bf93ed
Abofarhana <abofarhana88@gmail.com>
2022-06-28 08:10:09 -0700

    added all the files

--- portf.js
+++ portf.js
@@ -1,4 +1,46 @@
-let cardorig = 0;
+
+let cardPosition = 0;
 const cards = document.getElementsByClassName('card');
-const sumcards = cards.length;
-console.log(sumcards);
+const totalCards = cards.length;
+console.log(totalCards);
+
+document.
+getElementById("carousel_button--next").addEventListener("click", function() {
+    getTheNextCard();
+});
+    
+
+
+document.
+getElementById("carousel_button--prev").addEventListener("click", function() {
+    getThePrevCard();
+});
+
+
+function updateCards(){
+    for(let card of cards){
+        card.classList.remove("card--active");
+        card.classList.add("card");
+    }
+    cards[cardPosition].classList.add("card--active");
+}
+        
+
+function getTheNextCard(){
+        if(cardPosition == totalCards - 1){
+        cardPosition = 0;
+    }else{
+        cardPosition++;
+    }
+    updateCards();
+}
+
+function getThePrevCard(){
+      if(cardPosition == totalCards - 1){
+        cardPosition = 0;
+    }else{
+        cardPosition--;
+    }
+    updateCards();
+}
+
